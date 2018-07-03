302af7cf0d9265fbcbfe8edba07b7c6fc22e7bd2
Girish400 <girish400@gmail.com>
2018-07-03 07:43:39 +0530

    Update sbin.py

--- Redis/sbin.py
+++ Redis/sbin.py
@@ -41,4 +41,52 @@ df.set_index('timestamp',inplace=True,drop=True)
 ohlc = df['LTP'].resample('1Min').ohlc()
 
 print(ohlc)
+
+
+#########################################################################################################################    
+import redis
+import pandas as pd
+pd.set_option("precision", 2)
+
+
+r4 = redis.StrictRedis(host="localhost", port=6379,
+                       charset="utf-8", decode_responses=True, db=4)
+
+
+def dataframe():
+    data = r4.lrange("11709442", 0, -1)
+    i = 0; k = 0;  my_dict = {}
+    cols = ["instrument_token", "timestamp", "LTP", "volume"]
+    while i < r4.llen("11709442"):
+        tick = data[i].split(";")
+        d = dict(zip(cols, tick))
+        my_dict[k] = d
+        k += 1
+        i += 1
+
+    df = pd.DataFrame.from_dict(my_dict, 'index')
+    df.drop(df.tail(4).index, inplace=True)  # drop last n rows
+    return df
+
+
+def candles(df):
+    df = df.astype(dtype={"volume": "int32", "LTP": "float32",
+              "instrument_token": "int32", "timestamp": 'datetime64[ns]'})    
+    df.set_index('timestamp', inplace=True, drop=True)
+    ohlc = df['LTP'].resample('1Min').ohlc()
+    return ohlc
+
+
+def main():
+    df = dataframe()
+    print(df.tail(30))
+    print(candles(df))
+    
+
+if __name__ == '__main__':
+    main()
+    
+#########################################################################################################################    
+
+
     
