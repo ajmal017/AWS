4bb39a3dc542e28e6478190ef9e6dffdb1734df2
girish400@gmail.com <girish400@gmail.com>
2018-07-24 17:21:33 -0700

    working

--- Python/JSON.py
+++ Python/JSON.py
@@ -1,7 +1,6 @@
 # [JSON](http://www.json.org/)
 
 ### Example Python script returns a boolean if a string is valid json:
-
 import json
 
 def is_json(myjson):  
@@ -11,16 +10,16 @@ def is_json(myjson):
     return False  
   return True 
 
-### Which prints: 
+### Which prints:  
 
-print is_json("{}")                          #prints True
-print is_json("{asdf}")                      #prints False
-print is_json('{ "age":100}')                #prints True
-print is_json("{'age':100 }")                #prints False
-print is_json("{\"age\":100 }")              #prints True
-print is_json('{"age":100 }')                #prints True
-print is_json('{"foo":[5,6.8],"foo":"bar"}') #prints True
-Convert a JSON string to a Python dictionary:
+print(is_json("{}"))                          #prints True
+print (is_json("{asdf}"))                      #prints False
+print (is_json('{ "age":100}'))                #prints True
+print (is_json("{'age':100 }"))                #prints False
+print (is_json("{\"age\":100 }"))              #prints True
+print (is_json('{"age":100 }'))                #prints True
+print (is_json('{"foo":[5,6.8],"foo":"bar"}')) #prints True
+#Convert a JSON string to a Python dictionary:
 
 import json
 mydict = json.loads('{"foo":"bar"}')
@@ -29,9 +28,9 @@ print(mydict['foo'])    #prints bar
 mylist = json.loads("[5,6,7]")
 print(mylist)
 [5, 6, 7]
-Convert a python object to JSON string:
+#Convert a python object to JSON string:
 
 foo = {}
 foo['gummy'] = 'bear'
 print(json.dumps(foo))           #prints {"gummy": "bear"}
-If you want access to low-level parsing, don't roll your own, use an existing library: http://www.json.org/
+#If you want access to low-level parsing, don't roll your own, use an existing library: http://www.json.org/
