c6954d9937124bc58a9118e52fd533ede30fd609
Girish400 <girish400@gmail.com>
2019-01-08 23:53:02 +0530

    Update BulkQuotes.py

--- Redis/BulkQuotes.py
+++ Redis/BulkQuotes.py
@@ -4,8 +4,14 @@ import json
 import requests
 h = m.helperclass()
 
+df = pd.DataFrame()
 data = requests.get("https://api.kite.trade/quote/ltp?api_key={}&access_token={}&i=NSE:WIPRO&i=NSE:INFY&i=BSE:SENSEX&i=NSE:NIFTY+50".format(h.kite.api_key,h.kite.access_token)) 
 if data.status_code == 200:
     d = json.loads(data.content.decode('utf-8'))
     df = pd.DataFrame(list(d['data'].values()))
-df
+elif data.status_code == 403:
+    print("403 Error")
+else:
+    print("Error Getting Quote")
+if not df.empty:
+    print(df)
