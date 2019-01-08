f866b492a1b3ecde74f60ab8691d0eb65593d2b6
Girish400 <girish400@gmail.com>
2019-01-09 00:36:26 +0530

    Update BulkQuotes.py

--- Redis/BulkQuotes.py
+++ Redis/BulkQuotes.py
@@ -4,11 +4,27 @@ import json
 import requests
 h = m.helperclass()
 
+## LTP
 df = pd.DataFrame()
 data = requests.get("https://api.kite.trade/quote/ltp?api_key={}&access_token={}&i=NSE:WIPRO&i=NSE:INFY&i=BSE:SENSEX&i=NSE:NIFTY+50".format(h.kite.api_key,h.kite.access_token)) 
 if data.status_code == 200:
     d = json.loads(data.content.decode('utf-8'))
     df = pd.DataFrame(list(d['data'].values()))
+elif data.status_code == 403:
+    print("403 Error")
+else:
+    print("Error Getting Quote")
+if not df.empty:
+    print(df)
+
+## OHLC
+df = pd.DataFrame()
+data = requests.get("https://api.kite.trade/quote/ohlc?api_key={}&access_token={}&i=NSE:WIPRO&i=NSE:INFY&i=BSE:SENSEX&i=NSE:NIFTY+50".format(h.kite.api_key,h.kite.access_token)) 
+if data.status_code == 200:
+    df = pd.DataFrame(list(json.loads(data.content.decode('utf-8'))['data'].values()))
+    df1 = pd.DataFrame(list(df['ohlc']))
+    df= df.merge(df1,left_index=True,right_index=True)
+    df=df[['instrument_token', 'last_price','close', 'high', 'low','open']]
 elif data.status_code == 403:
     print("403 Error")
 else:
