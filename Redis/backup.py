f1aa1e59f401dec55017d6bea0dfa027a63a6dd8
girish400 <girish400@gmail.com>
2019-06-30 21:03:21 -0700

    Removed Redis

--- Redis/backup.py
+++
@@ -1,9 +0,0 @@
-while r4.llen(instrument_token):
-    data = r4.lpop(instrument_token)
-    r4.lpush("instrument_token1", data)
-    r4.lpush("instrument_token2", data)
-    
-
-while r4.llen('instrument_token2'): 
-    data = r4.lpop('instrument_token2')
-    r4.lpush("10504450", data)
